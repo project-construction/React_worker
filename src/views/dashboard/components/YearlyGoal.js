499a7b07712780288c07394d1e4968be1d0b987b
Jo Eunsang <sang11032@gmail.com>
2023-09-08 19:25:55 +0900

    K-dass

--- src/views/dashboard/components/YearlyGoal.js
+++ src/views/dashboard/components/YearlyGoal.js
@@ -14,7 +14,7 @@ const YearlyGoal = () => {
 
   return (
       <DashboardCard title="K-DASS 검진표">
-        <Link to="/button7">
+        <Link to="/survey">
           <img src={require("./설문.png")} alt="설문 이미지" width="100" height="100" style={{ display: "block", margin: "0 auto" }}/>
           <p></p>
           <Button className="custom-button" style={{ display: "block", margin: "0 auto" }}>
